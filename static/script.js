9b5bf5eb6537c08a571fbba3f401c844db7918cc
otaviotex <otaviodepaivaa@gmail.com>
2025-11-24 08:58:11 -0300

    Update script.js

--- static/script.js
+++ static/script.js
@@ -3,7 +3,6 @@ const VerEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 function validateFields() {
     const email = document.getElementById("email").value;
     const botao = document.getElementById("entrar");
-
     botao.disabled = !VerEmail.test(email);
 }
 
@@ -16,4 +15,19 @@ function validarFormulario() {
     }
 
     return true;
+}
+
+/* MÁSCARA DE TELEFONE */
+function mascaraTelefone(input) {
+    let valor = input.value.replace(/\D/g, "");
+
+    if (valor.length > 11) valor = valor.slice(0, 11);
+
+    if (valor.length > 6) {
+        input.value = `(${valor.slice(0,2)}) ${valor.slice(2,7)}-${valor.slice(7)}`;
+    } else if (valor.length > 2) {
+        input.value = `(${valor.slice(0,2)}) ${valor.slice(2)}`;
+    } else {
+        input.value = valor.replace(/^(\d{0,2})/, "($1");
+    }
 }
